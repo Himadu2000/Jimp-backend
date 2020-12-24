9e56f4d12e0c2276a7369664e1fc84d1891ffaea
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-12-25 01:16:15 +0530

    Working

---
+++ index.js
@@ -0,0 +1,38 @@
+var Jimp = require("jimp");
+const app = require("express")();
+var upload = require("multer")({ dest: "uploads/" });
+const helmet = require("helmet");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3000;
+
+// app.use(helmet());
+app.use(cors({ origin: "http://localhost:8000" }));
+// app.all("*", (req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", `http://localhost:8000`);
+//   next();
+// });
+
+app.post("/", upload.array("photos"), (req, res) => {
+  // console.log(req.files);
+  const processedImages = [];
+  req.files.forEach((file) => {
+    Jimp.read(file.path)
+      .then((image) => {
+        image.getBase64("image/png", (err, value) => {
+          processedImages.push(value);
+        });
+        if (req.files.length === processedImages.length) {
+          res.send(processedImages);
+        }
+      })
+      .catch((err) => console.error(err));
+  });
+  // res.set("Content-Type", "image/png");
+  // res.send(Buffer(processedImages));
+  // res.send(Buffer(processedImages));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
+});
