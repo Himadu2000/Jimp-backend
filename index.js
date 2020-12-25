379f2c68c50ef84726d2b6963ed41d15833f1ca5
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-12-25 11:18:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-var Jimp = require("jimp");
-const app = require("express")();
-var upload = require("multer")({ dest: "uploads/" });
-const helmet = require("helmet");
-const cors = require("cors");
+var Jimp = require("jimp")
+const app = require("express")()
+var upload = require("multer")({ dest: "uploads/" })
+const helmet = require("helmet")
+const cors = require("cors")
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-app.use(helmet());
-app.use(cors());
-// app.all("*", (req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", `http://localhost:8000`);
-//   next();
-// });
+app.use(helmet())
+app.use(cors())
+app.all("*", (req, res, next) => {
+  console.info(req.url)
+  next()
+})
 
 app.post("/", upload.array("photos"), (req, res) => {
   // console.log(req.files);
-  const processedImages = [];
-  req.files.forEach((file) => {
+  const processedImages = []
+  req.files.forEach(file => {
     Jimp.read(file.path)
-      .then((image) => {
+      .then(image => {
         image.getBase64("image/png", (err, value) => {
-          processedImages.push(value);
-        });
+          processedImages.push(value)
+        })
         if (req.files.length === processedImages.length) {
-          res.send(processedImages);
+          res.send(processedImages)
         }
       })
-      .catch((err) => console.error(err));
-  });
+      .catch(err => console.error(err))
+  })
   // res.set("Content-Type", "image/png");
   // res.send(Buffer(processedImages));
   // res.send(Buffer(processedImages));
-});
+})
 
 app.listen(PORT, () => {
-  console.log(`Server started at ${PORT}`);
-});
+  console.log(`Server started at ${PORT}`)
+})
